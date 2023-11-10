9543fd299972038d8893239f21813e71969b9a7a
Jessica Keller <jessica.keller68@gmail.com>
2023-11-10 17:59:02 +0100

    ajout de la page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,10 +6,42 @@ use App\Model\ContactManager;
 
 class ContactController extends AbstractController
 {
+
     public function index(): string
     {
         $contactManager = new ContactManager();
-        $contacts = $contactManager->selectAllContact();
-        return $this->twig->render('contact/contact.html.twig', ['contacts' => $contacts]);
+        $errors = [];
+        $validateContact = false;
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $contacts = array_map('trim', $_POST);
+            $errors = $this->validateContact($contacts);
+
+            if (empty($errors)) {
+                $contactManager->insert($contacts);
+                $validateContact = true;
+            }
+        }
+        return $this->twig->render('Contact/contact.html.twig', ['errors'=>$errors, 
+        'validateContact'=>$validateContact]);
+    }
+
+    public function validateContact(array $contacts): array
+    {
+        $errors = [];
+
+        if (empty($contacts['type'])) {
+            $errors['type'] = 'Veuillez sélectionner le type de demande';
+        }
+        if (empty($contacts['email'])) {
+            $errors['email'] = "Veuillez saisir votre adresse email";
+        }
+        if (!filter_var($contacts['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['validateEmail'] = 'L\'email n\'est pas valide';
+        }
+        if (empty($contacts['message'])) {
+            $errors['message'] = 'Veuillez saisir un message';
+        }
+        return $errors;
     }
 }
